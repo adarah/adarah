c411bd4a86191dfb6df5f433ca331f9935a061e6
Lucas Harada <lucasyharada@gmail.com>
2022-07-18 11:28:52 -0300

    fix bug where cpu would be invoked to execute 0 cycles

--- emulator/chip-8/src/main.zig
+++ emulator/chip-8/src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const wasm = @import("./wasm.zig");
+const util = @import("./util.zig");
 const c = @import("./consts.zig");
 const Cpu = @import("./cpu.zig").Cpu;
 const Keypad = @import("./keypad.zig").Keypad;
@@ -16,7 +17,7 @@ var delay_timer: Timer = undefined;
 var cpu_clock_speed_hz: c_int = undefined;
 var prev_time_ms: c_int = undefined;
 
-// This is the panic handler. Use util.panic for easier convenience.
+// This is the panic handler. Use util.panic for easier cdebugonvenience.
 pub fn panic(message: []const u8, trace: ?*std.builtin.StackTrace) noreturn {
     @setCold(true);
     if (builtin.os.tag == .freestanding) {
@@ -44,30 +45,8 @@ export fn init(clock_speed_hz: c_int, start_time_ms: c_int) void {
     });
 }
 
-fn keycodeToKeypad(keycode: c_int) !u4 {
-    return switch (keycode) {
-        c.KEY_1 => 0x1,
-        c.KEY_2 => 0x2,
-        c.KEY_3 => 0x3,
-        c.KEY_4 => 0xC,
-        c.KEY_Q => 0x4,
-        c.KEY_W => 0x5,
-        c.KEY_E => 0x6,
-        c.KEY_R => 0xD,
-        c.KEY_A => 0x7,
-        c.KEY_S => 0x8,
-        c.KEY_D => 0x9,
-        c.KEY_F => 0xE,
-        c.KEY_Z => 0xA,
-        c.KEY_X => 0x0,
-        c.KEY_C => 0xB,
-        c.KEY_V => 0xF,
-        else => error.UnknownKey,
-    };
-}
-
 export fn onKeydown(keycode: c_int) void {
-    const key = keycodeToKeypad(keycode) catch return;
+    const key = util.keycodeToKeypad(keycode) catch return;
     // As noted in the source below, the subroutine to get keyboard input should set the sound timer to 4,
     // which essentially makes the emulator play a sound while the key is being held down
     // https://laurencescotford.com/chip-8-on-the-cosmac-vip-keyboard-input/
@@ -77,7 +56,7 @@ export fn onKeydown(keycode: c_int) void {
 }
 
 export fn onKeyup(keycode: c_int) void {
-    const key = keycodeToKeypad(keycode) catch return;
+    const key = util.keycodeToKeypad(keycode) catch return;
     keypad.releaseKey(key);
     wasm.log("Released {d}", .{key});
 }
@@ -86,11 +65,18 @@ var global_frame: @Frame(Cpu.fetchDecodeExecute) = undefined;
 
 export fn onAnimationFrame(now_time_ms: c_int) void {
     const elapsed = now_time_ms - prev_time_ms;
-    wasm.log("now: {} | prev: {}", .{ now_time_ms, prev_time_ms });
-    const cpu_cycles = @divFloor(elapsed * cpu_clock_speed_hz, 1000);
-    wasm.log("Instructions to execute {d}", .{cpu_cycles});
+    const num_instructions = @divFloor(elapsed * cpu_clock_speed_hz, 1000);
+
+    // Due to intentional imprecisions in the timer functions in the browser,
+    // sometimes now is smaller than previous. Even if the time elapsed is positive,
+    // the next frame might be requested too soon (notice the divFloor)
+    if (num_instructions <= 0) {
+        return;
+    }
+
+    wasm.log("Instructions to execute {d}", .{num_instructions});
     var i: usize = 0;
-    while (i < cpu_cycles) : (i += 1) {
+    while (i < num_instructions) : (i += 1) {
         global_frame = async cpu.fetchDecodeExecute();
     }
     wasm.log("Executed all", .{});
