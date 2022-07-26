cb49ab0bf9ae26c1cbbf8d4162f06f06e4c2d3dd
Lucas Harada <lucasyharada@gmail.com>
2022-07-26 14:47:20 -0300

    apply prettier changes

--- blog/src/routes/index.svelte
+++ blog/src/routes/index.svelte
@@ -1,11 +1,18 @@
 <svelte:head>
 	<title>Home | Lucas Harada's blog</title>
 </svelte:head>
-<h1>Lucas Harada's blog</h1>
-<h2>A place for me to write down ideas, lessons, and whatever comes to mind.</h2>
-<figure data-testid="home-picture">
-  <img src="/azumarill-marill-beach.jpeg" alt="Azumarill and marill at a beach"/>
-  <figcaption>
-    <i>Image by Artsy Theo via <a href="https://artsy-theo.tumblr.com/post/180738351293/azumarill-and-marill-at-the-beach">Tumblr</a></i>
-  </figcaption>
-</figure>
+<article>
+	<header>
+		<h1>Lucas Harada's blog</h1>
+		<h2>A place for me to write down ideas, lessons, and whatever comes to mind.</h2>
+		<figure data-testid="home-picture">
+			<img src="/azumarill-marill-beach.jpeg" alt="Azumarill and marill at a beach" />
+			<figcaption>
+				Image by Artsy Theo via
+				<a href="https://artsy-theo.tumblr.com/post/180738351293/azumarill-and-marill-at-the-beach">
+					Tumblr
+				</a>
+			</figcaption>
+		</figure>
+	</header>
+</article>
